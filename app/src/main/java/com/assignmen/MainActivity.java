069285f7b8807b1cd26db121fce92e4de5f5abd8
Unknown <prabhukumar.swamy@gmail.com>
2017-12-27 23:42:11 +0530

    initcommit
    initcommit

---
+++ app/src/main/java/com/assignmen/MainActivity.java
@@ -0,0 +1,14 @@
+package com.assignmen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // initialising layout ( activity_main.xml )
+        setContentView(R.layout.activity_main);
+    }
+}
